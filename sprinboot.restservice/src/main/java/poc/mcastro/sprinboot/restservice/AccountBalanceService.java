2516d40d704052cf7527f7e3267fabdaa62d2cdb
mcastro <maucasco@gmail.com>
2016-04-13 17:32:15 -0500

    change the service add exception when not data

--- sprinboot.restservice/src/main/java/poc/mcastro/sprinboot/restservice/AccountBalanceService.java
+++ sprinboot.restservice/src/main/java/poc/mcastro/sprinboot/restservice/AccountBalanceService.java
@@ -3,9 +3,13 @@ package poc.mcastro.sprinboot.restservice;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
 import poc.mcastro.sprinboot.restservice.model.AccountTO;
 import poc.mcastro.sprinboot.restservice.model.SendMoneyTO;
 import poc.mcastro.sprinboot.restservice.repository.AccountsRepository;
@@ -17,26 +21,41 @@ public class AccountBalanceService {
 	AccountsRepository accountRepository;
 	
 		
-	@RequestMapping("addMoney")
+	@RequestMapping(value="addMoney", method = RequestMethod.POST)
 	public void addMoney(SendMoneyTO sendMoneyTO){
 		AccountTO accountTO=accountRepository.findByAccountNumber(sendMoneyTO.getAccountTO().getAccountNumber());
-		if(accountTO!=null){
-			accountTO.getAccountBalance().add(sendMoneyTO.getAmmount());
-			accountRepository.save(accountTO);
+		if(accountTO==null){
+			throw new EmptyResultDataAccessException(1);
+			
 		}
+		accountTO.getAccountBalance().add(sendMoneyTO.getAmmount());
+		accountRepository.save(accountTO);
 	}
-	@RequestMapping("getAccountBalance")
+	@RequestMapping(value="getAccountBalance", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
 	public BigDecimal getAccountBalance(String accountNumber){
 		AccountTO accountTO=accountRepository.findByAccountNumber(accountNumber);
-		if(accountTO!=null){
-			return accountTO.getAccountBalance();
+		if(accountTO==null){
+			throw new EmptyResultDataAccessException(1);
+			
 		}
-		return null;
+		return accountTO.getAccountBalance();
 	}
 	
-	@RequestMapping("getAccount")
+	@RequestMapping(value="getAccount", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
 	public AccountTO getAccount(String accountNumber){
-		
+		AccountTO accountTO=accountRepository.findByAccountNumber(accountNumber);
+		if(accountTO==null){
+			throw new EmptyResultDataAccessException(1);
+		}
 		return accountRepository.findByAccountNumber(accountNumber);
+		
+		  
+	}
+	   @ResponseStatus(HttpStatus.BAD_REQUEST)
+ @ExceptionHandler(value = {EmptyResultDataAccessException.class})
+	public void notFound(){
+		
 	}
 }
